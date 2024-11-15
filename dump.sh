2b29c2f69db2d4b6dd85f02951ff69eb21c99786
Paul Tuck <p6ul.tuck@gmail.com>
2024-11-15 21:20:54 +0700

    docs: Add project dump script

--- dump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -path "./env" -prune -o -type f \( -name "*.py" -o -name "*.js" -o -name "*.css" -o -name "*.html" -o -name "*.yaml" -o -name "*.json" -o -name "*.conf" -o -name "*.txt" \) \
-    -exec sh -c 'echo "\n==== $(basename {}) ====\n"; cat {}' \; > django-project-template-dump.txt
